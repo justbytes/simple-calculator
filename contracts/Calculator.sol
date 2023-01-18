2418677050eaa1e9abd7d9002cfe35510c762b8e
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-17 19:33:14 -0800

    add, subtract, multiply, divide functions added to contract, created directory for testing

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -3,5 +3,22 @@ pragma solidity ^0.8.9;
 
 
 contract Calculator {
+    // For testing purposes result will be set to 10 
+    int256 result = 10;
 
+    function add(int256 _num) public returns (int256) {
+        return result = result + _num;
+    }
+
+    function subtract(int256 _num) public returns (int256) {
+        return result = result - _num;
+    }
+
+    function multiply(int256 _num) public returns (int256) {
+        return result = result * _num;
+    }
+
+    function divide(int256 _num) public returns (int256) {
+        return result = result / _num;
+    }
 }
