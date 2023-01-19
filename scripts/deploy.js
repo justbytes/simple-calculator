357685ba5e2f6b183015b535801dad264dc7427b
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-19 10:54:00 -0800

    calculator contract now deploys to local host

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,15 @@
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
-async function main() {}
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  const Calculator = await ethers.getContractFactory("Calculator");
+  const calculator = await Calculator.deploy();
+  await calculator.deployed();
+
+  console.log(`Simple Calculator at: ${calculator.address}`);
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
