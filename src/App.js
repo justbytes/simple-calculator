90f47c7061dca97c27a058c1574eeacab1131ab9
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-19 12:21:08 -0800

    code added for connecting metamask account and displays account address on page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import "./style/App.css";
 
 // Imports components
@@ -6,8 +7,23 @@ import "./style/App.css";
 import Calculator from "./components/Calculator";
 
 function App() {
+  const [account, setAccount] = useState();
+  // Connect and retrive account from metamask
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
+  // Call for getting accounts
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
   return (
     <div>
+      {account}
       <Calculator />
     </div>
   );
