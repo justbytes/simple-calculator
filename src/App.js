af01c74be5563d3be1c8ef9da92f42e5f19fd317
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-19 15:36:26 -0800

    created abi and config json files, connected smart contract to front-end

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import "./style/App.css";
 
-// Imports components
-import Calculator from "./components/Calculator";
+// Components
+import CalculatorInterface from "./components/Calculator";
 import Navbar from "./components/Navbar";
 
+// ABI
+import Calculator from "./abis/Calculator.json";
+
+// Config
+import config from "./config.json";
+
 function App() {
+  const [provider, setProvider] = useState();
+  const [calculator, setCalculator] = useState();
   const [account, setAccount] = useState();
 
-  const loadBlockchainData = async () => {};
+  const loadBlockchainData = async () => {
+    //Connect to blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    //Get network
+    const network = await provider.getNetwork();
+
+    // Create JavaScript version of smart contract
+    const calculator = new ethers.Contract(
+      config[network.chainId].calculator.address,
+      Calculator,
+      provider
+    );
+    setCalculator(calculator);
+  };
 
   useEffect(() => {
     loadBlockchainData();
@@ -18,7 +40,7 @@ function App() {
   return (
     <div>
       <Navbar account={account} setAccount={setAccount} />
-      <Calculator />
+      <CalculatorInterface />
     </div>
   );
 }
