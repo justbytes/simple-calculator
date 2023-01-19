def24b1db2e1f93089b2918078c0685702223cd8
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-19 10:31:50 -0800

    numbered buttons now write to input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleClick = (event) => {
+    setInputValue(inputValue + event.target.textContent);
+  };
+
   return (
     <div className="calculator-body">
-      <input className="calculator-input"></input>
-      <button className="calculator-key">1</button>
-      <button className="calculator-key">2</button>
-      <button className="calculator-key">3</button>
-      <button className="calculator-key">4</button>
-      <button className="calculator-key">5</button>
-      <button className="calculator-key">6</button>
-      <button className="calculator-key">7</button>
-      <button className="calculator-key">8</button>
-      <button className="calculator-key">9</button>
-      <button className="calculator-key">0</button>
+      <input
+        className="calculator-input"
+        type="text"
+        id="input"
+        value={inputValue}
+        readOnly
+      />
+      <button className="calculator-key" onClick={handleClick}>
+        1
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        2
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        3
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        4
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        5
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        6
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        7
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        8
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        9
+      </button>
+      <button className="calculator-key" onClick={handleClick}>
+        0
+      </button>
       <button className="calculator-key">=</button>
-      <button className="calculator-key">CLR</button>
+      <button className="calculator-key" onClick={() => setInputValue("")}>
+        CLR
+      </button>
       <button className="calculator-key">+</button>
       <button className="calculator-key">-</button>
       <button className="calculator-key">X</button>
