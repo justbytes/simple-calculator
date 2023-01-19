6a53eb954c7a9cc04e76ebf14e956585d176f60f
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-19 13:10:44 -0800

    initial setup of navbar, connect and disconnect functionality for connecting wallet to site

--- src/App.js
+++ src/App.js
@@ -3,27 +3,21 @@ import { ethers } from "ethers";
 import "./style/App.css";
 
 // Imports components
-
 import Calculator from "./components/Calculator";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [account, setAccount] = useState();
-  // Connect and retrive account from metamask
-  const loadBlockchainData = async () => {
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    const account = ethers.utils.getAddress(accounts[0]);
-    setAccount(account);
-  };
-  // Call for getting accounts
+
+  const loadBlockchainData = async () => {};
+
   useEffect(() => {
     loadBlockchainData();
   }, []);
 
   return (
     <div>
-      {account}
+      <Navbar account={account} setAccount={setAccount} />
       <Calculator />
     </div>
   );
