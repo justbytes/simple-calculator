26bea2ddf7c6e1e3e3572bf0febef748ebed94a4
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-02-04 22:03:04 -0800

    contract deployed to goerli test net

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_PRIVATE_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
 };
